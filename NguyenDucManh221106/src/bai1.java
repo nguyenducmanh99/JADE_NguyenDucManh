2af3a9f6ed5b16900c7eaff905425ed537308727
nguyenducmanh99 <72304291+nguyenducmanh99@users.noreply.github.com>
2023-05-07 20:44:05 +0700

    update main

--- NguyenDucManh221106/src/bai1.java
+++ NguyenDucManh221106/src/bai1.java
@@ -7,11 +7,23 @@ public class bai1 {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Main 
       Runtime rt =  Runtime.instance();
       Profile p = new ProfileImpl();
       p.setParameter(Profile.MAIN, "localhost");
       p.setParameter(Profile.GUI, "true");
       AgentContainer ac = rt.createMainContainer(p);
+      
+      // Container 2
+      Profile p2 = new ProfileImpl();
+      p.setParameter(Profile.CONTAINER_NAME, "Container2");
+      p.setParameter(Profile.MAIN_HOST, "localhost");
+      AgentContainer ac2 = rt.createAgentContainer(p2);
+      
+      // Container 3
+      Profile p3 = new ProfileImpl();
+      p.setParameter(Profile.CONTAINER_NAME, "Container3");
+      p.setParameter(Profile.MAIN_HOST, "localhost");
+      AgentContainer ac3 = rt.createAgentContainer(p3);
 	}
 }
